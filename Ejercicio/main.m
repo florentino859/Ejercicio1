304133de8b735c848445772d1fbfd51f51b7f31c
CCDM18 <ccdm18@iMac-de-CCDM18.local>
2014-08-15 11:16:38 -0600

    kdsakdksakdakdkasd

--- Ejercicio/main.m
+++ Ejercicio/main.m
@@ -14,7 +14,66 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        NSString *mensaje = @"Hola a la calculadora";
+        NSLog(@"%@",mensaje);
+        
+        float primerValor;
+        scanf("%f",&primerValor);
+        
+        int opcion;
+        scanf("%d",&opcion);
+        
+        float segundoValor;
+        scanf("%f",&segundoValor);
+        
+        float operacion = 0;
+        
+        switch (opcion) {
+            case 0:
+                operacion = primerValor + segundoValor;
+                break;
+            case 1:
+                operacion = primerValor - segundoValor;
+                break;
+            case 2:
+                operacion = primerValor * segundoValor;
+                break;
+            case 3:
+                operacion = primerValor / segundoValor;
+                break;
+            
+            default:
+                break;
+        }
+        
+        if(operacion > 100){
+            NSLog(@"mensaje 1");
+        }else{
+            NSLog(@"mensaje 2");
+        }
+        
+        NSMutableArray *arrayOperaciones =[[NSMutableArray alloc] init];
+        
+        NSMutableArray * array;
+        array = [NSMutableArray alloc];
+        array = [NSMutableArray init];
+        
+        NSNumber *numero;
+        numero = [NSNumber alloc];
+        numero = [[NSNumber alloc] initWithFloat:operacion];
+        [array addObject:numero];
+        [array count];
+        
+        NSDictionary *dictionary = @{
+                                     @"nombre":@"florentino",
+                                     @"edad":@"120",
+                                     @"lenguajes":@"Objective-C"
+                                     };
+        
+        
+        
+    
         
     }
     return 0;
