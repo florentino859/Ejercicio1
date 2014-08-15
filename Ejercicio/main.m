4558e8abb896a765855b093f2d8ffe5ffe6ef034
CCDM18 <ccdm18@iMac-de-CCDM18.local>
2014-08-15 10:09:02 -0600

    Initial Commit

---
+++ Ejercicio/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Ejercicio
+//
+//  Created by CCDM18 on 15/08/14.
+//  Copyright (c) 2014 ___Joe___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
